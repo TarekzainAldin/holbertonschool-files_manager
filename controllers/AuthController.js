23d86f329752a91fe70d2bafd5bf8fe4149a9605
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-04-02 18:43:29 +0200

    authintcatino

---
+++ controllers/AuthController.js
@@ -0,0 +1,33 @@
+import sha1 from 'sha1';
+import { v4 as uuidv4 } from 'uuid';
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
+class AuthController {
+  static async getConnect(req, res) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Basic ')) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const base64Credentials = authHeader.split(' ')[1];
+    const credentials = Buffer.from(base64Credentials, 'base64').toString('utf-8');
+    const [email, password] = credentials.split(':');
+
+    if (!email || !password) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const user = await dbClient.getUserByEmail(email);
+    if (!user || user.password !== sha1(password)) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const token = uuidv4();
+    await redisClient.set(`auth_${token}`, user._id.toString(), 86400); // تخزين لمدة 24 ساعة
+
+    return res.status(200).json({ token });
+  }
+}
+
+export default AuthController;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
 
 const app = express.Router();
 app.use(express.json()); // Middleware to parse JSON request bodies
 
 // POST route to create a new user
 app.post('/users', UsersController.postNew);
+app.get('/connect', AuthController.getConnect);
+app.get('/disconnect', AuthController.getDisconnect);
+app.get('/users/me', UsersController.getMe);
 
 export default app;
