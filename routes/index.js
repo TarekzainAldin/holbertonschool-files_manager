b8544d21c9d517fc8ba03429f34f62eb7df222cb
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-04-02 18:20:00 +0200

    fis the esilte

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import UsersController from '../controllers/UsersController.js'; // Ensure you import the UsersController correctly
+import UsersController from '../controllers/UsersController';
 
 const app = express.Router();
 app.use(express.json()); // Middleware to parse JSON request bodies
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import routes from './routes/index.js'; // ✅ تأكد من استيراد `routes`
+import routes from './routes/index';
 
 const app = express();
 const port = process.env.PORT || 5000;
